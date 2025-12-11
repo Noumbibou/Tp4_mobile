27dcfcabe25df52be00e195b22cb6dd599091d9d
Noumbibou <williamsfomin62@gmail.com>
2025-12-11 11:23:54 +0100

    premier depot

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'; 
+import { View, Text, Button } from 'react-native'; 
+import { Ionicons } from '@expo/vector-icons';
+import HomeScreen from './screens/HomeScreen'; 
+import DetailsScreen from './screens/DetailsScreen'; 
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
+import SettingsScreen from './screens/SettingsScreen'; 
+import AppBar from './screens/AppBar';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const Stack = createNativeStackNavigator(); 
+const Tab = createBottomTabNavigator(); 
+
+function HomeStack() { 
+  return ( 
+    <Stack.Navigator screenOptions={{ headerShown: false }}> 
+      <Stack.Screen  name="Accueil"  component={HomeScreen}  options={{ 
+        headerStyle: { backgroundColor: '#007AFF' },    headerTintColor: '#fff',    headerTitleStyle: { 
+        fontWeight: 'bold' }, }} 
+      />  
+      <Stack.Screen  name="Details"  component={DetailsScreen}  options={{ title: 'Mes Détails Personnalisés' }} 
+      />  
+    </Stack.Navigator> 
   );
-}
+} 
+ 
+// --- Navigation par onglets --- 
+export default function App() { 
+  return (
+     <SafeAreaProvider>  
+    <NavigationContainer> 
+      <SafeAreaView style={{ backgroundColor: '#007AFF' }}> 
+          <AppBar />  
+        </SafeAreaView>
+      <Tab.Navigator 
+        screenOptions={{ 
+          headerShown: false,          
+          // cacher le header 
+          tabBarActiveTintColor: 'blue', // couleur de l’onglet actif 
+          tabBarInactiveTintColor: 'gray', // couleur de l’onglet inactif 
+          tabBarStyle: { backgroundColor: '#f0f0f0' }, // style de la barre 
+          tabBarLabelStyle: { fontSize: 14 }, // style du texte 
+        }} 
+      > 
+      <Tab.Screen  name="Maison"  component={HomeStack}  options={{ tabBarIcon: ({ color, size 
+          }) => (  <Ionicons name="home" size={size} color={color} />     ), 
+          }} /> 
+          <Tab.Screen name="Paramètres" component={SettingsScreen} options={{ tabBarIcon: ({ 
+          color,    size }) => ( <Ionicons name="settings" size={size} color={color} />  ), 
+          }}  
+      /> 
+      </Tab.Navigator> 
+    </NavigationContainer> 
+    </SafeAreaProvider> 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  ); 
+} 
---
+++ screens/SettingsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native'; 
+function SettingsScreen() { 
+  return ( 
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}> 
+      <Text>⚙  Paramètres</Text> 
+    </View> 
+  ); 
+} 
+export default SettingsScreen;
